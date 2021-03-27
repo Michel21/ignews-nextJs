2b060ee3f5af4833d453a899f99261bf5366f611
Michel Angelis Araujo da Rocha <michelmacpro@MBP-de-Michel.lan>
2021-03-26 21:11:13 -0300

    Titulo dinamico na pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import { Head } from 'next/document';
+
 import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+    <Head>
+       <title>Ignews</title>
+      </Head>
+     <h1 className={styles.title}>
+      Hello World
+    </h1>
+    </>
   )
 }
