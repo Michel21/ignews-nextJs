def6d3a2efda8d38b9372112a78a5e096d0d68cc
Michel Angelis Araujo da Rocha <michelmacpro@MBP-de-Michel.lan>
2021-03-27 12:54:07 -0300

    Created of component SubscriberButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscriberButton } from '../components/SubscriberButton';
 import styles from './home.module.scss';
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
           Get acess to all the publications <br/>
           <span>for $9.90 month</span>
         </p>
+        <SubscriberButton />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding"/>
