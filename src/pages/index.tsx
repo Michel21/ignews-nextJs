263c30761f2bf6ffb9a372a04afec64d9ec4c984
Michel Angelis Araujo da Rocha <michelmacpro@MBP-de-Michel.lan>
2021-03-27 11:33:27 -0300

    Created of component header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { Head } from 'next/document';
-
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-    <Head>
+     <Head>
        <title>Ignews</title>
-      </Head>
-     <h1 className={styles.title}>
-      Hello World
+     </Head>
+     <h1>
+       Hello World
     </h1>
     </>
   )
