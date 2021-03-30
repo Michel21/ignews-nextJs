00402028bf5eb40e5854763063937ed4e54eaac7
Michel Angelis Araujo da Rocha <michelmacpro@MBP-de-Michel.lan>
2021-03-30 10:23:09 -0300

    listener event of stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,8 +1,49 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await(const chunk of chunks) {
+    typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+ api:{
+   bodyParser: false
+ }
+}
+
+const relevantEvent = new Set([
+  'checkout.session.completed'
+])
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === 'POST'){
+    const buf = await buffer(req);
+    const secret = req.headers['stripe-signature'];
+
+    let event : Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (error) {
+      return res.status(400).send(`Webhooks error: ${error.message}`)
+    }
+
+    const type = event.type;
+
+    if(relevantEvent.has(type)){
+      res.json({received: true})
+    }
+  }else{
+    res.setHeader('Allow', 'POST');
+    res.status(400).end('Method not Allowed')
+  }
   
-  console.log('evento recebido');
-  
-  res.status(200).json({ok: true})
 }
